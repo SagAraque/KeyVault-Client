f78fe318ee58cc7efd5046df6f98a12aebf25781
SagAraque <sagaraque@gmail.com>
2023-04-09 16:53:28 +0200

    Included Run class and Manifest file

---
+++ src/main/java/com/example/keyvault_client/App.java
@@ -0,0 +1,10 @@
+package com.example.keyvault_client;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) {
+        ViewManager.main(args);
+    }
+}
