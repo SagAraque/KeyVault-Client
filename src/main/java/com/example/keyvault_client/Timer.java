71aa7986168cece221956d6f699ad692366595c0
SagAraque <sagaraque@gmail.com>
2023-04-16 18:40:09 +0200

    Timer time changed

--- src/main/java/com/example/keyvault_client/Timer.java
+++ src/main/java/com/example/keyvault_client/Timer.java
@@ -3,7 +3,7 @@ package com.example.keyvault_client;
 import javafx.application.Platform;
 
 public class Timer extends Thread{
-    private int timeSecs = 6000;
+    private int timeSecs = 600;
     private volatile boolean running = true;
 
     @Override
@@ -18,13 +18,14 @@ public class Timer extends Thread{
             if(running)
                 Platform.runLater(ViewManager::switchToLogin);
 
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void restartTimer(){
-        timeSecs = 6000;
+        timeSecs = 600;
     }
 
     public void stopThread(){
