d717922a034ee86f3d905d3b0ddcdccec65ec52e
SagAraque <sagaraque@gmail.com>
2023-04-22 21:18:36 +0200

    Init Internationalization

--- src/main/java/com/example/keyvault_client/ViewManager.java
+++ src/main/java/com/example/keyvault_client/ViewManager.java
@@ -10,6 +10,8 @@ import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,6 +19,7 @@ public class ViewManager extends Application {
     private static Stage window;
     public static ExecutorService executorService = Executors.newSingleThreadExecutor();
     public static ConnectionController conn = new ConnectionController();
+    public static ResourceBundle bundle;
 
     @Override
     public void start(Stage stage) {
@@ -31,6 +34,16 @@ public class ViewManager extends Application {
         Font.loadFont(ViewManager.class.getResourceAsStream("fonts/NotoSans-Regular.ttf"), 12);
         Font.loadFont(ViewManager.class.getResourceAsStream("fonts/NotoSans-Bold.ttf"), 12);
         System.setProperty("prism.lcdtext", "false");
+
+        Locale defaultLocale = Locale.getDefault();
+
+        if(defaultLocale.getLanguage().equals("es"))
+            defaultLocale = new Locale("es", "Es");
+        else
+            defaultLocale = new Locale("en", "Us");
+
+
+        bundle = ResourceBundle.getBundle("com.example.keyvault_client.lang.lang", defaultLocale);
 
         launch();
     }
@@ -50,7 +63,7 @@ public class ViewManager extends Application {
         try {
             restartExecutor();
 
-            FXMLLoader loader = new FXMLLoader(ViewManager.class.getResource("views/" + fxml));
+            FXMLLoader loader = new FXMLLoader(ViewManager.class.getResource("views/" + fxml), bundle);
             Scene scene = new Scene(loader.load(), width, height,true, SceneAntialiasing.BALANCED);
             Rectangle2D screenBounds = Screen.getPrimary().getBounds();
 
